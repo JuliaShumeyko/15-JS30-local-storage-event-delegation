b1c25f1c06a0d533f40c996714bf40442d3c53eb
Julia Shumeyko <46918344+JuliaShumeyko@users.noreply.github.com>
2019-09-29 22:55:34 +0300

    added checklist functionality

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || []; // on page load it is checked whether local storage has data, otherwise - empty array
 
+const addItem = (e) => {
+    e.preventDefault(); // prevents the page from reloading by default
+    const text = (e.target.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    }
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    e.target.reset();
+};
+
+const populateList = (plates = [], platesList) => {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+                <label for="item${i}">${plate.text}</label>
+            </li>
+            `;
+        }).join('');
+}
+
+const toggleDone = (e) => {
+    if (!e.target.matches('input')) return; //skip this unless it's an input
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+
+populateList(items, itemsList);
